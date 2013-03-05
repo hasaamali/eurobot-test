777b9332701406190172792224321fe5da4a752b
nick <nick@nicks-MacBook-Pro-2.local>
2013-03-05 19:46:26 +0000

    the code you are all looking for

---
+++ the_winning_code/the_winning_code.ino
@@ -0,0 +1,8 @@
+void setup() {
+  //so heres the code that will get you r robot ready to defat all others
+
+}
+
+void loop() {
+  // but this is where you will need to deliver on your promise....
+}
