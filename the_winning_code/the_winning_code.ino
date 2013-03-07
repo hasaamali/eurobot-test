da1de04e66c57caba101ae7e90eaed489d2f6173
pujmdx <p.varsani@mdx.ac.uk>
2013-03-07 14:51:13 +0000

    help

--- the_winning_code/the_winning_code.ino
+++ the_winning_code/the_winning_code.ino
@@ -6,7 +6,11 @@ void setup() {
 void loop() {
   // but this is where you will need to deliver on your promise....
   //still no real code
+<<<<<<< HEAD
   //oh my goodness what do
+=======
+  //oh my goodness what do we do now help
+>>>>>>> help
   //added this comment on a computer in the lab
   //does nothing
 }
